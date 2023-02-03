bf436861bf7e13faa21f44e7f33270e780b78200
Nithinreddy44 <122263554+Nithinreddy44@users.noreply.github.com>
2023-02-03 14:44:30 +0530

    Create n9.c

---
+++ n9.c
@@ -0,0 +1,19 @@
+//reversing a array
+#include<stdio.h>
+#define N 1000
+int main(){
+	int arr[N];
+int n ;
+printf("enter the size of the array:");
+scanf("%d",&n);
+printf("enter an array:");
+for(int i=0;i<n;i++){
+	scanf("%d",&arr[i]);
+}
+printf("reversed array:");
+for (int i=n-1;i>=0;i--){
+	printf("%d",arr[i]);
+	printf("");
+}
+return 0;
+}

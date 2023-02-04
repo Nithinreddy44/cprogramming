600e8724145893308c184dffb272122ed3444190
Nithinreddy44 <122263554+Nithinreddy44@users.noreply.github.com>
2023-02-04 12:30:17 +0530

    Create n25.c

---
+++ n25.c
@@ -0,0 +1,20 @@
+//vowel
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+main()
+{
+	char str[MAX];
+	int a,vowel,consonant,i;
+	printf("enter a string");
+	scanf("%[^\n]s",&str);
+	vowel=0;
+	consonant=0;
+	a=strlen(str);
+	for(i=0;i<a;i++)
+	{
+		if(str[i]=='a' || str[i]=='e' ||str[i]=='i' ||str[i]=='o' ||str[i]=='u' ||str[i]=='A' ||str[i]=='E' ||str[i]=='I' ||str[i]=='O' ||str[i]=='U')
+		vowel++;
+	}
+	printf("total number of vowel=%d\n",vowel);
+}

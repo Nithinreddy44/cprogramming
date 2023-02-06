0fe2f8b99595df8aa6503b895f6a67e535baf352
Nithinreddy44 <122263554+Nithinreddy44@users.noreply.github.com>
2023-02-05 23:54:01 -0800

    Create n58.c

---
+++ n58.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define N 1000
+int main(){
+int arr[N];
+int n;
+printf("ENTER THE SIZE OF THE ARRAY:");
+scanf("%d",&n);
+printf("enter an array:");
+for(int i=0;i<n;i++){
+scanf("%d",&arr[i]);
+}
+printf("reversed array:");
+for (int i = n-1; i>=0; i--){
+printf("%d",arr[i]);
+printf(" ");
+}
+return 0;
+}

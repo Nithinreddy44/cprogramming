8131f0805b7fc67547af4925bfde0d41dfeaa09b
Nithinreddy44 <122263554+Nithinreddy44@users.noreply.github.com>
2023-02-06 10:03:06 +0530

    Create n34.c

---
+++ n34.c
@@ -0,0 +1,11 @@
+// student and staff number in clg
+#include <stdio.h>
+int main(){
+	int a,b,c,d;
+	printf("enter the total number of user:");
+	scanf("%d",&a);
+	printf("enter the number of teaching staff : ");
+	scanf("%d",&b);
+	printf("student users:%d",a-b-(b/3));
+	}
+   
